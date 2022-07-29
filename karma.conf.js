cc0d4ed30df373bfc04e46162512bd8c76fe4200
AmrDarrag <96830421+AmrDarrag@users.noreply.github.com>
2022-07-29 18:14:58 +0200

    Add files via upload

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/graduation'),
+      dir: require('path').join(__dirname, './coverage/products'),
       subdir: '.',
       reporters: [
         { type: 'html' },
