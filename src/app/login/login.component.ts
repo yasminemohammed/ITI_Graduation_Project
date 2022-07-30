496d84d915b881955e9493e9d56610ec59bba757
AmrDarrag <96830421+AmrDarrag@users.noreply.github.com>
2022-07-30 20:59:32 +0200

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,7 +9,10 @@ export class LoginComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
+    
+    submitfun(loginform : any) {
+        console.log(loginform);
+    }
 
 }
